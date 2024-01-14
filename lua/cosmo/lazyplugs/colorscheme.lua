430c8597774f5fca4c42f1c48f6a942a208f272c
cosmo <atticuswaterbury@gmail.com>
2024-01-14 14:09:00 -0800

    fixed theming and status line

--- lua/cosmo/lazyplugs/colorscheme.lua
+++ lua/cosmo/lazyplugs/colorscheme.lua
@@ -9,7 +9,7 @@ return {
             light = "latte",
             dark = "mocha",
         },
-        transparent_background = false, -- disables setting the background color.
+        transparent_background = true, -- disables setting the background color.
         show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
         term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
         dim_inactive = {
@@ -41,6 +41,8 @@ return {
         gitsigns = true,
         treesitter = true,
         notify = false,
+        harpoon = true,
+        telescope = true,
         mini = {
             enabled = true,
             indentscope_color = "",
