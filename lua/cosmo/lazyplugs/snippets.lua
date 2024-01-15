f19dad41b3d5c151be332848716bd7f67d6e1f30
cosmo <atticuswaterbury@gmail.com>
2024-01-14 20:09:02 -0800

    more snippets

--- lua/cosmo/lazyplugs/snippets.lua
+++ lua/cosmo/lazyplugs/snippets.lua
@@ -4,7 +4,24 @@ return {
     dependencies = {
         "rafamadriz/friendly-snippets",
     },
-    config = function ()
-        require("luasnip.loaders.from_vscode")
-    end
+    config = function()
+        local types = require 'luasnip.util.types'
+
+        require('luasnip.loaders.from_vscode').lazy_load()
+
+        require('luasnip').setup {
+            history = true,
+            delete_check_events = 'TextChanged',
+            -- Display a cursor-like placeholder in unvisited nodes
+            -- of the snippet.
+            ext_opts = {
+                [types.insertNode] = {
+                    unvisited = {
+                        virt_text = { { '|', 'Conceal' } },
+                        virt_text_pos = 'inline',
+                    },
+                },
+            },
+        }
+    end,
 }
